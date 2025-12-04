a28fffa323ff3745a601ddf39ee8f290ae372ec5
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-12-03 22:33:48 -0300

    feat: adicionar arquivos pessoas.ts e inscricao.ts

---
+++ src/types/inscricao.ts
@@ -0,0 +1,10 @@
+// src/types/inscricao.ts
+
+export interface Inscricao {
+    name: string;
+    age: number;
+    city: string;
+    phone: string;
+    paid: "Sim" | "Não Encontrado";
+    value: string;
+}
